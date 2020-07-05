f891168684212907efad926d71c7824c6e65ecf4
Gunjan Kothari <gunjan.kothari2002@gmail.com>
2020-07-05 13:03:52 +0530

    build

--- stencil.config.ts
+++ stencil.config.ts
@@ -6,11 +6,7 @@ export const config: Config = {
   taskQueue: 'async',
   outputTargets: [
     {
-      type: 'dist-custom-elements-bundle',
-    },
-    {
       type: 'dist',
-      esmLoaderPath: '../loader'
     },
     {
       type: 'docs-readme'
